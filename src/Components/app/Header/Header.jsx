2a56322ed4ef5d67b9e98a90142400ecaf6b2286
Alexsandr Pavlov <buterfeed@gmail.com>
2025-03-25 18:25:59 +0300

    fix page rated

--- src/Components/app/Header/Header.jsx
+++ src/Components/app/Header/Header.jsx
@@ -4,11 +4,19 @@ import {AppMenu} from '../../ui-components/AppMenu/AppMenu';
 
 const Header = (props) => {
   const {menuPage, setMenuPage, query, setQuery} = props;
-  return (
-    <header className="header-app">
-      <AppMenu menuPage={menuPage} setMenuPage={setMenuPage} />
-      <SearchFilm query={query} setQuery={setQuery} />
-    </header>
-  );
+  if (menuPage === 'rated') {
+    return (
+      <header className="header-app">
+        <AppMenu menuPage={menuPage} setMenuPage={setMenuPage} />
+      </header>
+    );
+  } else {
+    return (
+      <header className="header-app">
+        <AppMenu menuPage={menuPage} setMenuPage={setMenuPage} />
+        <SearchFilm query={query} setQuery={setQuery} />
+      </header>
+    );
+  }
 };
 export default Header;
